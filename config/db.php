4f91c415fb4612a37594d7a963dda90d3c7910e9
Dima <anglosax@mail.ru>
2016-12-16 15:58:49 +0200

    add db config

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=testphp',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
